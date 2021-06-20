56ccb2e3a3b26cccfbc3aa6d0b9c08a6e67bf653
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-06-20 08:47:42 -0300

    Testing reponse to The Social Network request
    - Added request to the movie The Social Newtork
    - Added test to the properties "Title", "Year" and "Language" of The Social Newtwork movie request
    - Added global variables fo movie id and api key

--- cypress/integration/omdbapi.spec.js
+++ cypress/integration/omdbapi.spec.js
@@ -1,1 +1,15 @@
 /// <reference types="cypress" />
+
+const ID_DO_FILME = "tt1285016";
+const API_KEY = "52ec71bf";
+
+describe("Testes com a OMDb API", ()=>{
+	it("Request do filme The Social Network", ()=>{
+		cy.request("/?i="+ID_DO_FILME+"&apikey="+API_KEY).then((response)=>{
+			expect(response.body).property("Title").to.equal("The Social Network");
+			expect(response.body).property("Year").to.equal("2010");
+			expect(response.body).property("Language").to.contains("English");
+			expect(response.body).property("Language").to.contains("French");
+		});
+	});
+});
